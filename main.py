19796666325b58cb1ca31d978cd70f43f1d70060
OLAQI <137159097+OLAQI@users.noreply.github.com>
2025-02-24 00:59:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from astrbot import Bot, Plugin
-from astrbot.store import DictStore
-
-class WechatPatPlugin(Plugin):
-    def __init__(self, bot: Bot):
-        super().__init__(bot)
-        self.store = DictStore("wechat_pat_counts")  # 使用内置持久化存储[<sup>2</sup>](https://github.com/Soulter/AstrBot/wiki/四、开发插件)[<sup>4</sup>](https://blog.51cto.com/u_15483555/13267318)
+@plugin.on_message(keywords=["拍了拍我"])
+async def handle_pat(session: MessageSession):
+    user_id = session.ctx.user_id  # 获取用户ID[<sup>2</sup>](https://github.com/Soulter/AstrBot/wiki/四、开发插件)[<sup>4</sup>](https://blog.51cto.com/u_15483555/13267318)
     
-    async def handle_pat(self, event):
-        user_id = event.user_id
-        count = await self.store.get(user_id, 0) + 1
-        
-        replies = [
-            "别拍啦！",
-            "哎呀，还拍呀，别闹啦！",
-            "别拍我啦  你要做什么  不理你了",
-            "已宕机，请勿要，谢谢配合"
-        ]
-        
-        response = replies[min(count-1, 3)]  # 取前四次响应规则[<sup>1</sup>](https://astrbot.app/)
-        await self.send(event, response)
-        await self.store.set(user_id, count if count < 4 else 4)  # 计数上限设为4
+    # 初始化用户状态
+    if user_id not in user_states:
+        user_states[user_id] = 0
+    user_states[user_id] += 1  # 计数器递增[<sup>2</sup>](https://github.com/Soulter/AstrBot/wiki/四、开发插件)
 
-def setup(bot):
-    bot.register_plugin(WechatPatPlugin(bot))  # 注册插件[<sup>2</sup>](https://github.com/Soulter/AstrBot/wiki/四、开发插件)[<sup>5</sup>](https://blog.csdn.net/puterkey/article/details/145337107)
+    # 根据次数返回不同响应
+    count = user_states[user_id]
+    if count == 1:
+        await session.reply("别拍啦！")
+    elif count == 2:
+        await session.reply("哎呀，还拍呀，别闹啦！")
+    elif count == 3:
+        await session.reply("别拍我啦 你要做什么 不理你了")
+    else:
+        await session.reply("已宕机，请勿要，谢谢配合")
+        user_states[user_id] = 0  # 重置计数器[<sup>2</sup>](https://github.com/Soulter/AstrBot/wiki/四、开发插件)
